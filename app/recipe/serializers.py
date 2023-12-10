2958b04eb893e11b1ce71c7adf019acacbabc24f
testpageAN <alexis.nikolopoulos@hotmail.com>
2023-12-10 10:02:11 +0200

    build media and static lesson-129

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -102,6 +102,16 @@ class RecipeDetailSerializer(RecipeSerializer):
         fields = RecipeSerializer.Meta.fields + ['description']
 
 
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
+
+
 # class TagSerializer(serializers.ModelSerializer):
 #     """Serializer for tags."""
 #
